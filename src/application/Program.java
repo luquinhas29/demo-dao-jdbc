e1f2da0feb453cf3b83b963d8501484a36bf0da2
Lucas williams <lucaswilliamscesar@gmail.com>
2023-11-26 19:21:53 -0300

    findById implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,18 @@
 package application;
 
-import java.util.Date;
-
-import entities.Department;
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Books");
-		Seller seller = new Seller(21, "Lucas", "lucas@gmail.com", new Date(), 3000.00, department);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findByID(3);
 		System.out.println(seller);
+		
 
 	}
 
