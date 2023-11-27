5912503f755ffbe82b325d03c3d51d25a8f80b56
Lucas williams <lucaswilliamscesar@gmail.com>
2023-11-27 10:56:08 -0300

    Insert implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import dao.DaoFactory;
@@ -25,6 +26,7 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
 
 		System.out.println();
 		System.out.println("=== TEST 3 : seller findAll ===");
@@ -33,6 +35,10 @@ public class Program {
 			System.out.println(obj);
 		}
 
+	
+	System.out.println("=== TEST 4 : seller findInsert ===");
+	Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+	sellerDao.insert(newSeller);
+	System.out.println("Inserted! New id = "+ newSeller.getId());
 	}
-
 }
